df512acf05837c308a1ab8b8f4eb2e62f0de07da
Fulaji Thakor <fulaji.thakor@drcsystems.com>
2023-07-06 12:00:44 +0530

    upload all files

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -5,7 +5,6 @@ const userSchema = new Schema<UserModel>({
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: {type: String, required: true},
-    role:{type:String, enum: ['admin', 'manager', 'employee'], required:true}
 });
 
 const User = model<UserModel>('User', userSchema);
